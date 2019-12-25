61d84ae20b4ec27482dcc139f72c6b7b7c10f6cf
Rojesh04 <rojesh123mote@gmail.com>
2019-12-25 20:45:22 +0545

    okay

---
+++ intro.js
@@ -0,0 +1,24 @@
+
+<html>
+<head>
+	
+	<title>start</title> 
+	<script type="text/javascript" src="start.js">
+		function sendMail() {
+		var mail=sendMail({sender='rojesh',receiver='aakash',subject='project',message='jaypubg'})
+		ac
+		return  {
+
+			message:message
+		}
+	};
+
+	var roj =sendMail();
+	console.log('roj>>',roj);
+	
+	</script>
+</head>
+<body>
+<button onclick="myFunction()">Click me</button>
+</body>
+</html>
